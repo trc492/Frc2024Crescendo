06703216b297c52e08d0b013072c278d45f33494
Anand Krishnan <anandkrishnan1835@gmail.com>
2024-02-15 17:16:24 -0800

    Wrote basic outline AutoPickup

--- src/main/java/team492/autotasks/TaskAutoPickupFromGround.java
+++ src/main/java/team492/autotasks/TaskAutoPickupFromGround.java
@@ -23,11 +23,15 @@
 package team492.autotasks;
 
 import TrcCommonLib.trclib.TrcAutoTask;
+import TrcCommonLib.trclib.TrcDbgTrace;
 import TrcCommonLib.trclib.TrcEvent;
 import TrcCommonLib.trclib.TrcOwnershipMgr;
 import TrcCommonLib.trclib.TrcRobot;
 import TrcCommonLib.trclib.TrcTaskMgr;
+import TrcCommonLib.trclib.TrcUtil;
+import TrcFrcLib.frclib.FrcPhotonVision;
 import team492.Robot;
+import team492.vision.PhotonVision.PipelineType;
 
 /**
  * This class implements auto-assist task.
@@ -39,20 +43,29 @@ public class TaskAutoPickupFromGround extends TrcAutoTask<TaskAutoPickupFromGrou
     public enum State
     {
         START,
+        DETECT_NOTE, 
+        PREP_TO_DRIVE,
+        DRIVE_TO_NOTE,
         DONE
     }   //enum State
 
     private static class TaskParams
     {
-        TaskParams()
+        boolean inAuto;
+        TaskParams(boolean inAuto)
         {
+            this.inAuto = inAuto;
         }   //TaskParams
     }   //class TaskParams
 
     private final String ownerName;
     private final Robot robot;
+    private final TrcEvent event;
 
+
+    private final TrcDbgTrace msgTracer;
     private String currOwner = null;
+    private String driveOwner = null;
 
     /**
      * Constructor: Create an instance of the object.
@@ -60,11 +73,13 @@ public class TaskAutoPickupFromGround extends TrcAutoTask<TaskAutoPickupFromGrou
      * @param ownerName specifies the owner name to take subsystem ownership, can be null if no ownership required.
      * @param robot specifies the robot object that contains all the necessary subsystems.
      */
-    public TaskAutoPickupFromGround(String ownerName, Robot robot)
+    public TaskAutoPickupFromGround(String ownerName, Robot robot, TrcDbgTrace msgTracer)
     {
         super(moduleName, ownerName, TrcTaskMgr.TaskType.POST_PERIODIC_TASK);
         this.ownerName = ownerName;
         this.robot = robot;
+        this.msgTracer = msgTracer;
+        this.event = new TrcEvent(moduleName + ".event");
     }   //TaskAutoPickupFromGround
 
     /**
@@ -72,10 +87,13 @@ public class TaskAutoPickupFromGround extends TrcAutoTask<TaskAutoPickupFromGrou
      *
      * @param completionEvent specifies the event to signal when done, can be null if none provided.
      */
-    public void autoAssistPickup(TrcEvent completionEvent)
+    public void autoAssistPickup(TrcEvent completionEvent, boolean inAuto)
     {
-        tracer.traceInfo(moduleName, "event=" + completionEvent);
-        startAutoTask(State.START, new TaskParams(), completionEvent);
+        tracer.traceInfo(
+            moduleName, 
+            "event=" + completionEvent + 
+            ", inAuto=" + inAuto);
+        startAutoTask(State.START, new TaskParams(inAuto), completionEvent);
     }   //autoAssistPickup
 
     /**
@@ -83,7 +101,11 @@ public class TaskAutoPickupFromGround extends TrcAutoTask<TaskAutoPickupFromGrou
      */
     public void autoAssistCancel()
     {
-        tracer.traceInfo(moduleName, "Canceling auto-assist.");
+        final String funcName = "autoAssistCancel";
+
+        if(msgTracer != null){
+            tracer.traceInfo(moduleName, "Canceling auto-assist.");
+        }
         stopAutoTask(false);
     }   //autoAssistCancel
 
@@ -101,6 +123,7 @@ public class TaskAutoPickupFromGround extends TrcAutoTask<TaskAutoPickupFromGrou
     @Override
     protected boolean acquireSubsystemsOwnership()
     {
+        // TODO: Figure out subsystem owenership aquisition
         boolean success = ownerName == null ||
                           (robot.robotDrive.driveBase.acquireExclusiveAccess(ownerName));
 
@@ -129,6 +152,7 @@ public class TaskAutoPickupFromGround extends TrcAutoTask<TaskAutoPickupFromGrou
     @Override
     protected void releaseSubsystemsOwnership()
     {
+        // TODO: Figure out subsystem ownership release
         if (ownerName != null)
         {
             TrcOwnershipMgr ownershipMgr = TrcOwnershipMgr.getInstance();
@@ -139,6 +163,16 @@ public class TaskAutoPickupFromGround extends TrcAutoTask<TaskAutoPickupFromGrou
             robot.robotDrive.driveBase.releaseExclusiveAccess(currOwner);
             currOwner = null;
         }
+
+        if (driveOwner != null)
+        {
+            robot.robotDrive.driveBase.releaseExclusiveAccess(driveOwner);
+            driveOwner = null;
+        }
+
+
+        robot.intake.releaseExclusiveAccess(currOwner);
+        currOwner = null;
     }   //releaseSubsystemsOwnership
 
     /**
@@ -165,12 +199,52 @@ public class TaskAutoPickupFromGround extends TrcAutoTask<TaskAutoPickupFromGrou
     protected void runTaskState(
         Object params, State state, TrcTaskMgr.TaskType taskType, TrcRobot.RunMode runMode, boolean slowPeriodicLoop)
     {
-        // TaskParams taskParams = (TaskParams) params;
+        TaskParams taskParams = (TaskParams) params;
 
         switch (state)
         {
             case START:
+                // We are checking if the back camera is working, and if so, moving to the DETECT_NOTE state
+
+                if(robot.photonVisionBack != null){
+                    tracer.traceInfo(moduleName, "Using Vision to find Note.");
+                    sm.setState(State.DETECT_NOTE);
+                } else{
+                    // Back camera is not working, moving to DONE State
+                    tracer.traceInfo(moduleName, "Error in Camera, moving to DONE State");
+                    sm.setState(State.DONE);
+                }
+
                 break;
+
+            case DETECT_NOTE:
+                    
+                FrcPhotonVision.DetectedObject object = robot.photonVisionBack.getBestDetectedObject();
+                
+                if(object != null){
+                // TODO: Write Logic
+                // TODO: Logic needs review
+                    
+
+                    // double targetDist = TrcUtil.magnitude(object., object.targetPose.y);
+                    // double targetAngle = object.targetPose.
+
+
+                    
+                }
+                // TODO: Write State
+                break;
+
+            case PREP_TO_DRIVE:
+                // TODO: Write State
+                // Will tuck in shooter, and prep subsystems to pickup object
+                break;
+
+            case DRIVE_TO_NOTE:
+                // TODO: Write State
+                // Will implement driving code
+                break;
+
 
             default:
             case DONE:
